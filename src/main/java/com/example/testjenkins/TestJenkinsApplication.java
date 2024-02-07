7891965f86fc1f2a359ed0b5854c0ce4fd303911
BilelTn <bilelbenabdallah31@gmail.com>
2024-02-07 01:53:34 +0100

    second commit

--- src/main/java/com/example/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -9,6 +9,7 @@ public class TestJenkinsApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
         System.out.println("hello from jenkins test spring boot");
+        System.out.println("test apres le push et declenchement automatique des build");
     }
 
 }
