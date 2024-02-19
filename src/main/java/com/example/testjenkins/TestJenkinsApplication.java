fd0b74c565adcfdc6e8d1b9d7ba74e80b1112e84
BilelTn <bilelbenabdallah31@gmail.com>
2024-02-19 22:04:24 +0100

    test2

--- src/main/java/com/example/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -1,7 +1,9 @@
 package com.example.testjenkins;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.env.ConfigTreePropertySource;
 
 @SpringBootApplication
 public class TestJenkinsApplication {
