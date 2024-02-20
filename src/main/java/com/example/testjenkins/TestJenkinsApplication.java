c20a341745472c420ac5e9c71c015245e0b93847
BilelTn <bilelbenabdallah31@gmail.com>
2024-02-20 21:35:27 +0100

    test3

--- src/main/java/com/example/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -10,7 +10,7 @@ public class TestJenkinsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestJenkinsApplication.class, args);
-        System.out.println("hello from jenkins test and githookspring boot");
+        System.out.println("hello  jenkins test and githookspring boot");
         System.out.println("test apres le push  declenchement automatique des build");
 
         System.out.println("test apres  le push web hook et sonar scan");
