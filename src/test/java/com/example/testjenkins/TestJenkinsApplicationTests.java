85fc512eb92132bbebd27d11befaad69cbb61b17
ahmed <ahmedbrini206@gmail.com>
2024-02-24 12:58:14 +0100

    commit Ahmed 22

--- src/test/java/com/example/testjenkins/TestJenkinsApplicationTests.java
+++ src/test/java/com/example/testjenkins/TestJenkinsApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.testjenkins;
 
 import com.example.testjenkins.entity.User;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 public class TestJenkinsApplicationTests {
-    static User user,user1;
+    static User user,user1,user2,user3;
 
 
     @BeforeAll
@@ -15,13 +19,16 @@ public class TestJenkinsApplicationTests {
         System.out.println("les teste ont commance");
         user=new User("ahmed","ahmedbrini@gmail.com");
         user1=new User("ahmed","ahmedbrini@gmail.com");
+
     }
 
     @Test
+    @DisplayName("teste user name et user Email")
     void contextLoads() {
 
-        boolean teste=user.equals(user1);
-        assert (teste);
+       boolean teste=user.equals(user1);
+       assert (teste);
+
     }
 
 }
